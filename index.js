1c33611c590b7f86c5dbfc11eeff05838a5cfb7a
QaziSaboorr <qazi.ali@ucalgary.ca>
2023-05-27 03:24:24 -0600

    alternate

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ $(document).ready(function () {
   let x = true;
   let o = false;
   let arrayRecord = [];
+  let firstturn = true;
+  let ok = false;
+
   let player1 = {
     name: "PLayer 1",
     score: 0,
+    turn: true,
   };
   let player2 = {
     name: "PLayer 2",
     score: 0,
+    turn: false,
   };
   let tie = 0;
   $("#start").click(function (event) {
@@ -27,9 +32,7 @@ $(document).ready(function () {
         $("#tie").fadeIn(2000);
         $("#scoretracker1").css("display", "block");
         $("#scoretracker2").css("display", "block");
-
         player1.name = $("#name1").val();
-
         player2.name = $("#name2").val();
         if ($("#name1").val() === "") {
           player1.name = "Player 1";
@@ -68,22 +71,36 @@ $(document).ready(function () {
     numberOfturns = 0;
     $("#fun").text("");
     won = false;
-    x = true;
-    o = false;
     arrayRecord = [];
-    $("#fun").text(player1.name + "'s turn");
+
     for (let i = 0; i < 9; i++) {
       $("#" + i.toString()).empty();
       console.log(i.toString());
       $("#" + i.toString()).css("background", "red");
     }
+    if (x === true) {
+      x = false;
+      o = true;
 
+      $("#fun").text(player1.name + "'s turn");
+      let temp = player2;
+      player2 = player1;
+      player1 = temp;
+    } else {
+      x = true;
+      o = false;
+      $("#fun").text(player2.name + "'s turn");
+
+      let temp = player1;
+      player1 = player2;
+      player2 = temp;
+    }
     start();
   });
   function start() {
-    if (won === false) {
+    if (won == false) {
       $(".boxes").click(function () {
-        if (x === true) {
+        if (x === true && won == false) {
           let idx = $(this).attr("id");
           let idxtransform = `#` + idx;
           if (!$(idxtransform).text()) {
@@ -92,10 +109,11 @@ $(document).ready(function () {
             !$(idxtransform).text("x");
             arrayRecord[parseInt(idx)] = "x";
             numberOfturns++;
+
             $("#fun").text(player2.name + "'s turn");
             check("x", player1);
           }
-        } else if (o === true) {
+        } else if (o === true && won == false) {
           let ido = $(this).attr("id");
           let idotransform = `#` + ido;
           if (!$(idotransform).text()) {
@@ -197,6 +215,7 @@ $(document).ready(function () {
       $("#fun").text(player.name + " Won");
       $("#restart").css("display", "block");
       ++player.score;
+      ok = true;
 
       $("#scoretracker1").text(paragraph());
     }
